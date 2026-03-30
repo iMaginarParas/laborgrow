ee05aa307c55c67e49d3c3caaf91f6f87d2bae6c
darshanurs <darshanurs54@gmail.com>
2026-03-30 19:57:53 +0530

    chore: remove test files and logs

--- check_jobs_cols_v2.py
+++
@@ -1,8 +0,0 @@
-from database import init_supabase
-c = init_supabase()
-r = c.table('jobs').select('*').limit(1).execute()
-if r.data:
-    for key in sorted(r.data[0].keys()):
-        print(f"COL: {key}")
-else:
-    print("No Data")
